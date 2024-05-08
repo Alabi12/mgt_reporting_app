3911055755850245e0f2ccf81105429bca23c512
Robert O. Alabi <wolo22001@gmail.com>
2024-05-08 15:52:21 +0000

    add devise and setup the login credentials

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :reports
   root "reports#index"
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  # delete "users/sign_out" => "devise/sessions#destroy", as: :destroy_user_session
+  resources :reports
 end
