2ee878692b4a2313e414549174d8f68e1976d435
Robert O. Alabi <wolo22001@gmail.com>
2024-05-13 16:38:45 +0000

    update the master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = 
+  
+  RAILS_ENV=production bundle exec rake assets:precompile
+
+
+  # config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
