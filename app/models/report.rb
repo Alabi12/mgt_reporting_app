62e3d731c91cf8cba3cd7b065b1c8106d8156042
Robert O. Alabi <wolo22001@gmail.com>
2024-05-19 23:10:36 +0000

    update the controller and the model files

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,9 @@
 class Report < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
 
-    enum status: { ongoing: 0, completed: 1, not_started: 2 }
+  # Define enum with more descriptive names to avoid conflicts
+  enum status: { ongoing: 0, completed: 1, not_started_report: 2 }
 
-  validates :status, presence: true # Validate presence of status
+  validates :status, presence: true
 end
+
