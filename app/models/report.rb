c780564ac05f3769f824eee08d87e2b3cbb8d599
Robert O. Alabi <wolo22001@gmail.com>
2024-05-20 00:38:15 +0000

    fixed enums error to add attribute to database

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :user
 
+  attribute :status, :integer 
+
   # Define enum with more descriptive names to avoid conflicts
   enum status: { ongoing: 0, completed: 1, not_started_report: 2 }
 
