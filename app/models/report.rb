cd25518111b9a63b30d99cf5cd56f830349b4a35
Robert O. Alabi <wolo22001@gmail.com>
2024-05-20 01:00:42 +0000

    fixed group and attendancde attribute in the model

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,6 +5,8 @@ class Report < ApplicationRecord
 
   attribute :group, :string
 
+  attribute :attendance, :integer
+
   # Define enum with more descriptive names to avoid conflicts
   enum status: { ongoing: 0, completed: 1, not_started_report: 2 }
 
